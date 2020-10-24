3e39a4b690c4f60a1dad9425a081dc3b5497fc2d
NiurbisReyesJaquez <71194774+NiurbisReyesJaquez@users.noreply.github.com>
2020-10-24 16:21:44 -0400

    Add project files.

---
+++ TareaOrdenesDetalle/DAL/Contexto.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TareaOrdenesDetalle.Models;
+
+namespace TareaOrdenesDetalle.DAL
+{
+    public class Contexto :DbContext
+    {
+        public DbSet<Suplidores> Suplidores { get; set; }
+        public DbSet<Productos> Productos { get; set; }
+        public DbSet<Ordenes> Ordenes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data source = Data/OrdenesDetalle.db");
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Suplidores>().HasData(new Suplidores
+            {
+                SuplidorId = 1,
+                Nombre = "Almacen Juan Maria"
+            });
+
+            modelBuilder.Entity<Suplidores>().HasData(new Suplidores
+            {
+                SuplidorId = 2,
+                Nombre = "Surtidora La Economica"
+            });
+
+            modelBuilder.Entity<Productos>().HasData(new Productos
+            {
+                ProductoId = 1,
+                Descripcion = "Malta Morena",
+                Costo = 300,
+                Inventario = 40,
+            });
+
+            modelBuilder.Entity<Productos>().HasData(new Productos
+            {
+                ProductoId = 2,
+                Descripcion = "Chocolate Hersheys",
+                Costo = 500,
+                Inventario = 15,
+            });
+        }
+    }
+}
---
+++ TareaOrdenesDetalleTests/BLL/OrdenesBLLTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TareaOrdenesDetalle.BLL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TareaOrdenesDetalle.Models;
+
+namespace TareaOrdenesDetalle.BLL.Tests
+{
+    [TestClass()]
+    public class OrdenesBLLTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            bool paso = false;
+            Ordenes orden = new Ordenes();
+
+            orden.OrdenId = 0;
+            orden.Fecha = DateTime.Now;
+            orden.SuplidorId = 1;
+            orden.OrdenesDetalle = new List<OrdenesDetalle>();
+            orden.Monto = 100;
+
+            paso = OrdenesBLL.Guardar(orden);
+            Assert.IsTrue(paso);
+        }
+
+        [TestMethod()]
+        public void ExisteTest()
+        {
+            bool paso = false;
+            Ordenes orden = new Ordenes();
+
+            orden.OrdenId = 1;
+            orden.Fecha = DateTime.Now;
+            orden.SuplidorId = 1;
+            orden.OrdenesDetalle = new List<OrdenesDetalle>();
+            orden.Monto = 5;
+
+            paso = OrdenesBLL.Modificar(orden);
+            Assert.AreEqual(paso, true);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
